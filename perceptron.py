63c27021a824e446683554dd41d439de678bcdf9
Prem Patel <rolexhere03@gmail.com>
2026-03-10 00:20:41 +0530

    1

---
+++ perceptron.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class perceptron:
+    def __init__(self,):
+        pass
+
+    def sigmoid(self,):
+        pass
+
+    def predict(self):
+        pass
+
+    def fit():
+        pass
+
+    def evaluate():
+        pass
